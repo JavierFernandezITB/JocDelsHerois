86f25a48047d86a55400143bb77412723ce96d08
JavierFernandezITB <javier.fernandez.7e7@itb.cat>
2024-01-22 18:35:38 +0100

    closes #1

--- JocDelsHerois/Program.cs
+++ JocDelsHerois/Program.cs
@@ -3,6 +3,7 @@ using System;
 using Menu;
 using Heroes;
 using Utils;
+using Messages;
 
 namespace JocDelsHeroisNamespace
 {
@@ -11,6 +12,34 @@ namespace JocDelsHeroisNamespace
         public static void Main()
         {
 
+            const int MaxTries = 3;
+            int CurrentMenuTries = 1;
+
+            bool exitMainMenu = false;
+            bool startMainGame = false;
+
+            while (!exitMainMenu)
+            {
+                int choice;
+                Console.WriteLine(MessagesList.MainMenuWelcomeMessage);
+                Console.WriteLine(MessagesList.MainMenuOptionsMessage);
+                choice = Convert.ToInt32(Console.ReadLine());
+                while (!UtilsHandler.IsInRange(choice, 0, 1) && CurrentMenuTries < MaxTries)
+                {
+                    CurrentMenuTries++;
+                    Console.Clear();
+                    Console.WriteLine(MessagesList.MainMenuErrorNotInRange);
+                    Console.WriteLine(MessagesList.MainMenuOptionsMessage);
+                    choice = Convert.ToInt32(Console.ReadLine());
+                }
+
+                if (choice == 1)
+                    startMainGame = true;
+                else if (CurrentMenuTries >= MaxTries)
+                    Console.WriteLine(MessagesList.ErrorNoTriesLeft);
+
+                exitMainMenu = true;
+            }
         }
     }
 }
