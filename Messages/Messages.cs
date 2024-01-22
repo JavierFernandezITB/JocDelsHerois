0c7772ce861f821e56f8f4bdf6b22554967be640
JavierFernandezITB <javier.fernandez.7e7@itb.cat>
2024-01-22 21:26:40 +0100

    closes #3

--- Messages/Messages.cs
+++ Messages/Messages.cs
@@ -6,5 +6,12 @@
         public const string MainMenuOptionsMessage = "1. Empezar una nueva batalla\n0. Salir del juego";
         public const string MainMenuErrorNotInRange = "Vaya, te has equivocado. Vuelve a intentarlo!";
         public const string ErrorNoTriesLeft = "Te has quedado sin intentos.";
+
+        public const string DifficultySelectionMessage = "Selecciona una dificultad.";
+        public const string DifficultySelectionOptions = "1. Fácil\n2. Difícil\n3. Personalizado\n4. Random";
+
+        public const string DifficultyCustomChangeStat = "Bien, vamos a cambiar la {0} de {1}. [{2} - {3}]";
+
+        public const string ChangedHeroStatMessage = "Se han actualizado valores. [OLD] {0} -> [NEW] {1}";
     }
 }
